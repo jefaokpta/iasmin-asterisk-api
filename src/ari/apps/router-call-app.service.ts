61314bd28bdaa98ef4dd6fcf711c7c30d4e7cb15
jefaokpta <jefaokpta@hotmail.com>
2024-11-08 18:54:34 -0300

    ligacoes rolando!

--- src/ari/apps/router-call-app.service.ts
+++ src/ari/apps/router-call-app.service.ts
@@ -3,38 +3,77 @@
  * Date: 11/8/24
  */
 
-import { Client, connect } from 'ari-client';
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { Bridge, Channel, Client, connect, StasisStart } from 'ari-client';
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RouterCallAppService implements OnApplicationBootstrap {
-  constructor(private readonly configService: ConfigService) {
-  }
+  constructor(private readonly configService: ConfigService) {}
 
   onApplicationBootstrap() {
     connect(
       this.configService.get('AST_HOST')!,
       this.configService.get('AST_USER')!,
       this.configService.get('AST_PASS')!,
-      this.clientLoaded,
+      this.ariReady,
     );
   }
 
-  clientLoaded(error: Error, client: Client) {
-    if (error) {
-      throw error;
+  ariReady(error: Error, ari: Client) {
+    if (error) throw error.message;
+
+    function stasisStart(stasisStartEvent: StasisStart, channel: Channel) {
+      const company = stasisStartEvent.args[1];
+      Logger.log(`Channel ${channel.name} entrou no Stasis Company ${company}`, 'RouterCallAppService');
+      if (company) originateDialedChannel(channel);
     }
-    console.log('Connected to ARI!!!!!!!!!!!!!!!!!');
 
-    client.channels.list(function (err, channels) {
-      if (err) {
-        throw err;
-      }
+    function originateDialedChannel(channel: Channel) {
+      channel.ring((err) => {if (err) throw err.message});
+      const dialedChannel = ari.Channel();
 
-      channels.forEach(function (channel) {
-        console.log(channel.name);
+      dialedChannel.on('StasisStart', (stasisStartEvent: StasisStart, dialedChannel: Channel) => {
+        createBridgeForChannels(channel, dialedChannel);
+      })
+
+      dialedChannel.originate({
+          endpoint: `PJSIP/101#${channel.dialplan.exten}@TWILLIO_JUPITER`,
+          timeout: 30,
+          app: 'router-call-app',
+          appArgs: 'dialed',
+          callerId: channel.dialplan.exten,
+          variables: {
+            'PJSIP_HEADER(add,P-Asserted-Identity)': '100023',
+          }},
+        (err) => {if (err) {Logger.error(err.message, 'RouterCallAppService')}},
+      );
+    }
+
+    function createBridgeForChannels(channel: Channel, dialedChannel: Channel) {
+      const bridge = ari.Bridge();
+
+      channel.answer((err) => {if (err) throw err.message});
+
+      bridge.create({type: 'mixing'}, (err, bridge) => {
+        if (err) throw err.message;
+        Logger.log(`Bridge ${bridge.id} criada`, 'RouterCallAppService');
       });
-    });
+
+      addChannelsToBridge(channel, dialedChannel, bridge);
+    }
+
+    function addChannelsToBridge(channel: Channel, dialedChannel: Channel, bridge: Bridge) {
+      bridge.addChannel({channel: [channel.id, dialedChannel.id]}, (err) => {
+        if (err) throw err.message;
+        Logger.log(`Canais ${channel.id} e ${dialedChannel.id} adicionados à bridge ${bridge.id}`, 'RouterCallAppService');
+      });
+
+    }
+    //todo: tratar desligamentos de chamadas de ambos os lados
+    ari.on('StasisStart', stasisStart)
+
+    ari.start('router-call-app')
+      .then(() => Logger.log('Roteador de chamadas: router-call-app 🚀', 'RouterCallAppService'))
   }
 }
