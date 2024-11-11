da10d47f4a4e8ff43566cc97294d2181c6b62cff
jefaokpta <jefaokpta@hotmail.com>
2024-11-11 14:30:37 -0300

    refatoracao de funcoes

--- src/ari/apps/router-call-app.service.ts
+++ src/ari/apps/router-call-app.service.ts
@@ -3,9 +3,10 @@
  * Date: 11/8/24
  */
 
-import { Bridge, Channel, Client, connect, StasisStart } from 'ari-client';
+import { Channel, Client, connect, StasisStart } from 'ari-client';
 import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { originateDialedChannel } from './util/utlis';
 
 @Injectable()
 export class RouterCallAppService implements OnApplicationBootstrap {
@@ -26,80 +27,7 @@ export class RouterCallAppService implements OnApplicationBootstrap {
     function stasisStart(stasisStartEvent: StasisStart, channel: Channel) {
       const company = stasisStartEvent.args[1];
       Logger.log(`Channel ${channel.name} entrou no Stasis Company ${company}`, 'RouterCallAppService');
-      if (company) originateDialedChannel(channel);
-    }
-
-    function originateDialedChannel(channel: Channel) {
-      channel.ring((err) => {if (err) throw err.message});
-      const dialedChannel = ari.Channel();
-
-      channel.on('StasisEnd', () => {
-        originalChannelHangup(channel, dialedChannel);
-      })
-
-      dialedChannel.on('ChannelDestroyed', () => {
-        dialedChannelHangup(channel, dialedChannel);
-      })
-
-      dialedChannel.on('StasisStart', (stasisStartEvent: StasisStart, dialedChannel: Channel) => {
-        createBridgeForChannels(channel, dialedChannel);
-      })
-
-      dialedChannel.originate({
-          endpoint: `PJSIP/101#${channel.dialplan.exten}@TWILLIO_JUPITER`,
-          timeout: 30,
-          app: 'router-call-app',
-          appArgs: 'dialed',
-          callerId: channel.dialplan.exten,
-          variables: {
-            'PJSIP_HEADER(add,P-Asserted-Identity)': '100023',
-          }},
-        (err) => {if (err) {Logger.error(err.message, 'RouterCallAppService')}},
-      );
-    }
-
-    function createBridgeForChannels(channel: Channel, dialedChannel: Channel) {
-      const bridge = ari.Bridge();
-
-      channel.answer()
-        .catch((err) => {if (err) throw err.message});
-
-      dialedChannel.on('StasisEnd', () => {
-        bridgeDestroy(bridge, dialedChannel);
-      })
-
-      bridge.create({type: 'mixing'})
-        .then((bridge) => {
-          Logger.log(`Bridge ${bridge.id} criada`, 'RouterCallAppService');
-          addChannelsToBridge(channel, dialedChannel, bridge);
-        })
-        .catch((err) => {if (err) throw err.message});
-    }
-
-    function addChannelsToBridge(channel: Channel, dialedChannel: Channel, bridge: Bridge) {
-      bridge.addChannel({channel: [channel.id, dialedChannel.id]}, (err) => {
-        if (err) throw err.message;
-        Logger.log(`Canais ${channel.id} e ${dialedChannel.id} adicionados à bridge ${bridge.id}`, 'RouterCallAppService');
-      });
-
-    }
-
-    function originalChannelHangup(channel: Channel, dialedChannel: Channel) {
-      Logger.log(`Canal ${channel.name} desligou, desligando ${dialedChannel.name}`, 'RouterCallAppService')
-      dialedChannel.hangup()
-        .catch((err) => Logger.error('Canal Original desligado', err.message, 'RouterCallAppService'));
-    }
-
-    function dialedChannelHangup(channel: Channel, dialedChannel: Channel) {
-      Logger.log(`Canal ${dialedChannel.name} desligou, desligando ${channel.name}`, 'RouterCallAppService')
-      channel.hangup()
-        .catch((err) => Logger.error('Canal Discado desligado', err.message, 'RouterCallAppService'));
-    }
-
-    function bridgeDestroy(bridge: Bridge, dialedChannel: Channel) {
-      Logger.log(`Canal discado ${dialedChannel.name} desligou, Bridge ${bridge.id} será destruída`, 'RouterCallAppService')
-      bridge.destroy()
-        .catch((err) => Logger.error(`Bridge do canal ${dialedChannel.name} destruída`, err.message, 'RouterCallAppService'));
+      if (company) originateDialedChannel(ari, channel);
     }
 
     ari.on('StasisStart', stasisStart)
