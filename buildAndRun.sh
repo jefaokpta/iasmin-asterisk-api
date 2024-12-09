5b74acfa00b99829ceda4d4c3076fef38d2da9a9
jefaokpta <jefaokpta@hotmail.com>
2024-12-09 16:15:49 -0300

    logs automatizando buildAndRun.sh

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Building and running Iasmin Asterisk Manager"
+
 git pull
 npm run build
 rm /var/log/iasmin-asterisk-api.log
 systemctl restart iasmin-asterisk-manager
+
+echo "Iasmin Asterisk Manager is running"
