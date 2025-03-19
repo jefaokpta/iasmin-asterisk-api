71ed1ff80e8094f4bacbd3f16205615d7b38f02c
jefaokpta <jefaokpta@hotmail.com>
2025-03-19 11:20:30 -0300

    Corrige o log de ligações no método stasisStart para exibir o valor correto do token da chamada.

--- src/ari/apps/router-call-app.service.ts
+++ src/ari/apps/router-call-app.service.ts
@@ -42,7 +42,7 @@ export class RouterCallAppService implements OnApplicationBootstrap {
   private async stasisStart(stasisStartEvent: StasisStart, channel: Channel, ari: Client) {
     const company = stasisStartEvent.args[1];
     const callToken = await channel.getChannelVar({variable: 'PJSIP_HEADER(read,X-CALL-TOKEN)'});
-    Logger.log(`Ligacao de ${channel.name} ${channel.caller.name} para ${channel.dialplan.exten} Empresa ${company} - token ${callToken}`, 'RouterCallAppService');
+    Logger.log(`Ligacao de ${channel.name} ${channel.caller.name} para ${channel.dialplan.exten} Empresa ${company} - token ${callToken.value}`, 'RouterCallAppService');
     if (!company) return;
     if (channel.dialplan.exten === '12345') {
       externalMediaCall(ari, channel);
