3b884ce3ca94ce715063b63ed6d95ab4041a90e4
jefaokpta <jefaokpta@hotmail.com>
2025-04-03 17:20:31 -0300

    "Melhora script buildAndRun.sh com logs e reinício automático"

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Building and running Iasmin Asterisk Manager"
+echo "🚀 Building and running Iasmin Asterisk Manager"
 
+echo "🔄 Sincronizando com GitHub"
 git pull
+
+echo "📦 Instalando dependências"
 npm install
+
+echo "🛠️ Buildando o projeto"
 npm run build
+
+echo "🧹 Limpando logs antigos"
 rm /var/log/iasmin-asterisk-api.log
+
+echo "🔄 Reiniciando serviço Iasmin Asterisk API"
 systemctl restart iasmin-asterisk-api
 
-echo "Iasmin Asterisk API is running"
+echo "✅ Iasmin Asterisk API is running"
