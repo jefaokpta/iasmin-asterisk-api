38d4a1add8d44f237e94a9ea5bbf8c01896a7c64
jefaokpta <jefaokpta@hotmail.com>
2025-05-12 13:56:11 -0300

    IA: Reativar exclusão de áudios, ajustar URLs e incluir upload de arquivos.
    - Reconhecimento: Reativada exclusão de áudios após a transcrição para otimizar armazenamento.
    - Frontend: Alterada URL para backend em produção e adicionado upload de arquivos MP3 no "call-analysis".
    - Upload: Logger ajustado e nome de arquivos atualizado para maior clareza.

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
 @Injectable()
 export class UploadService {
+  private readonly logger = new Logger(UploadService.name);
 
   constructor(private readonly configService: ConfigService) {}
 
@@ -13,17 +14,16 @@ export class UploadService {
       const newName = this.renameAudio(id);
       const tempPath = join(this.configService.get('AUDIO_RECORD')!, 'mp3s', newName);
       writeFileSync(tempPath, audio.buffer);
-      Logger.log('Arquivo salvo com sucesso!');
+      this.logger.debug(`Arquivo ${newName} salvo com sucesso!`);
       return newName;
     } catch (error) {
-      Logger.error(`Erro ao salvar arquivo: ${error}`);
+      this.logger.error(`Erro ao salvar arquivo: ${error}`);
       throw new HttpException('Erro ao salvar arquivo de áudio', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   private renameAudio(id: string) {
     const timestamp = Date.now();
-    return `${timestamp}-${id}.mp3`;
+    return `upload-${timestamp}-${id}.mp3`;
   }
-  
 }
