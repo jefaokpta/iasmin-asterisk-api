36e6901515a5fa6b5679f93489d6ffc43e2a4752
jefaokpta <jefaokpta@hotmail.com>
2025-08-26 17:08:42 -0300

    abordagem then IA: Move log e `dial` para dentro de `then` no canal B
    - Alterado fluxo para mover log e método `dial` do canal B para dentro do `.then` após configuração dos valores do canal.
    - Motivo: Garantir execução ordenada e melhorar rastreabilidade no processo de chamada.

--- src/ari/calls/external-call.service.ts
+++ src/ari/calls/external-call.service.ts
@@ -60,10 +60,12 @@ export class ExternalCallService {
 
     await this.callAction.addChannelsToBridge(bridgeMain, [channelA.id, channelB.id]);
     await this.callAction.setChannelVar(channelB, 'PJSIP_HEADER(add,P-Asserted-Identity)', controlNumber);
-    await this.callAction.setChannelVar(channelB, 'CONNECTEDLINE(all)', ddr);
+    this.callAction.setChannelVar(channelB, 'CONNECTEDLINE(all)', ddr).then(() => {
+      this.logger.debug(`${channelA.id} >> Executando external dial para ${channelB.name}`);
+      channelB.dial({ timeout: 30 });
+    });
 
-    this.logger.debug(`${channelA.id} >> Executando external dial para ${channelB.name}`);
-    channelB.dial({ timeout: 30 });
+
   }
 
   private channelBAnsweredCall(channelA: Channel, channelB: Channel, bridgeMain: Bridge, ari: Client, ariApp: string) {
